04325e4a01c0d1523de506a4fbfcf23e923d051e
Elmer Marquez <leomarqz.main@gmail.com>
2026-09-22 11:35:45 -0600

    Se recreo el entorno de desarrollo y se actualizo la contrasenia de la base de datos, adicional se agrego la tabla y modelo User

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -12,6 +12,7 @@ namespace EcommerceApi.Data
 
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
+        public DbSet<User> Users { get; set; }
 
     }
 }
